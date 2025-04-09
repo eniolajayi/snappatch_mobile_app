a5af39ca8263396e62210e0c0b5094642bc77d16
eniolajayi <ajayieniola16@gmail.com>
2025-04-09 16:30:20 +0100

    update initial _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,56 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { TamaguiProvider } from "tamagui";
+import { type AppStateStatus, Platform } from "react-native";
+import {
+	focusManager,
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query";
+import { useOnlineManager } from "@/hooks/useOnlineManager";
+import { useAppState } from "@/hooks/useAppState";
+import tamaguiConfig from "@/tamagui.config";
+
+SplashScreen.preventAutoHideAsync();
+
+function onAppStateChange(status: AppStateStatus) {
+	if (Platform.OS !== "web") {
+		focusManager.setFocused(status === "active");
+	}
+}
+
+const queryClient = new QueryClient({
+	defaultOptions: { queries: { retry: 2 } },
+});
 
 export default function RootLayout() {
-  return <Stack />;
+	useOnlineManager();
+	useAppState(onAppStateChange);
+
+	const [loaded] = useFonts({
+		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+	});
+
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
+
+	if (!loaded) {
+		return null;
+	}
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<TamaguiProvider config={tamaguiConfig} defaultTheme="light">
+				<StatusBar style="auto" />
+				<Stack />
+			</TamaguiProvider>
+		</QueryClientProvider>
+	);
 }
